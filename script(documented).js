6126609054dca53011826c2699ad3f41b6ab4ae4
EURosales <eduardo_rosales26@outlook.com>
2021-03-10 23:11:26 -0600

    Se optimizó el código.

--- script(documented).js
+++ script(documented).js
@@ -1,181 +1,184 @@
 //Declaración de variables
 //Variable para botón de verificación
-const btnVerificar = document.getElementById('btnVerificar');
-const btnReload = document.getElementById('btnRetry');
+const btnVerificar = document.getElementById("btnVerificar");
+const btnReload = document.getElementById("btnRetry");
 //Variable para input que contiene el numero
-const numero = document.querySelector('#numTarjeta');
-const form = document.getElementById('myForm');
-const titulo = document.getElementById('titulo_informativo')
+const numero = document.querySelector("#numTarjeta");
+const form = document.getElementById("myForm");
+const titulo = document.getElementById("titulo_informativo");
 //avisos sobre problemas de validaciones
-const sm_empty = document.getElementById('smE');
-const sm_noMatch = document.getElementById('smN');
+const sm_empty = document.getElementById("smE");
+const sm_noMatch = document.getElementById("smN");
 
 btnReload.addEventListener("click", function (e) {
-    location.reload();
+  location.reload();
 });
 
 btnVerificar.addEventListener("click", function (e) {
-    //evita que entre el valor vacio por defecto a la primera función.
-    e.preventDefault();
-    /*Esta función contiene la función que valida si el input esta vacio
+  //evita que entre el valor vacio por defecto a la primera función.
+  e.preventDefault();
+  /*Esta función contiene la función que valida si el input esta vacio
     o no, dependiendo de eso se van ejecutando mas funciones hasta obtener el
     resultado.*/
-    getNumber();
+  getNumber();
 });
 
 //esta funcion va a contener el algoritmo principal
 function getNumber() {
-    checkEmpty();
-    //console.log(08570420000389851 % 10);
+  checkEmpty();
+  //console.log(08570420000389851 % 10);
 }
 
-    /*Función que valida que el input no este vacio, de lo contrario
+/*Función que valida que el input no este vacio, de lo contrario
     se muestra un error hasta que sea rellenado.*/
-    /*Si el campo no esta vacio, procede a ejecutar la función que
+/*Si el campo no esta vacio, procede a ejecutar la función que
     valida el contenido del input.*/
 function checkEmpty() {
-    /*Si el input resulta vacio, se muestra la pista de error para
+  /*Si el input resulta vacio, se muestra la pista de error para
     el usuario*/
-    if (numero.value == "") {
-        //aviso de error en consola
-        console.log("No has ingresado nada en el campo.");
-        //Pista visual de error para el usuario
-        sm_empty.classList.remove('hide');
-    } else {
-        /*Si se comprueba que el input NO esta vacio,
+  if (numero.value == "") {
+    //aviso de error en consola
+    console.log("No has ingresado nada en el campo.");
+    //Pista visual de error para el usuario
+    sm_empty.classList.remove("hide");
+  } else {
+    /*Si se comprueba que el input NO esta vacio,
         se procede a revisar su contenido*/
-        checkPattern();
-        //se oculta la pista visual de error
-        sm_empty.classList.add('hide');
-    }
+    checkPattern();
+    //se oculta la pista visual de error
+    sm_empty.classList.add("hide");
+  }
 }
 
-    /*Función para validar el correcto orden y estructura
+/*Función para validar el correcto orden y estructura
     de ingreso de los dígitos de la tarjeta de credito*/
 function checkPattern() {
-    /*se le asigna el valor numerico del input a la variable "digitList",
+  /*se le asigna el valor numerico del input a la variable "digitList",
     para luego comparar el string completo con el patron*/
-    const digitList = numero.value;
-    
-    //Ejemplo de patrón: "9999[espacio]9999[espacio]9999[espacio]9999"
-    /*Si la cadena de texto confuerda con el patrón estabecido debe ocultar el
+  const digitList = numero.value;
+
+  //Ejemplo de patrón: "9999[espacio]9999[espacio]9999[espacio]9999"
+  /*Si la cadena de texto confuerda con el patrón estabecido debe ocultar el
     aviso de error si se esta mostrando por algún error previo.*/
-    /*y tambien debe ejecutar la función que procesa el texto para llevar a cabo el algoritmo*/
-    
-    if (digitList.match(/^([0-9]{4}[\s][0-9]{4}[\s][0-9]{4}[\s][0-9]{3,})$/)) {
-        //console.log(digitList);
-        sm_noMatch.classList.add('hide');
-        //aqui se va a ejecutar la función que lleva a cabo todo el algoritmo
-        //ese algoritmo debe contener el "Mostrar (de prueba por el momento)"
-        /*se debe enviar el parametro ya revisado por el match() del patrón
+  /*y tambien debe ejecutar la función que procesa el texto para llevar a cabo el algoritmo*/
+
+  if (digitList.match(/^([0-9]{4}[\s][0-9]{4}[\s][0-9]{4}[\s][0-9]{3,})$/)) {
+    //console.log(digitList);
+    sm_noMatch.classList.add("hide");
+    //aqui se va a ejecutar la función que lleva a cabo todo el algoritmo
+    //ese algoritmo debe contener el "Mostrar (de prueba por el momento)"
+    /*se debe enviar el parametro ya revisado por el match() del patrón
         y asi comenzar a usarlo. en este caso "digitList" (la lista de dígitos)*/
-        resultado(digitList);
-        /*Se resetea el formulario que contiene los datos de ingreso del digito de
+    resultado(digitList);
+    /*Se resetea el formulario que contiene los datos de ingreso del digito de
         la tarjeta para poder seguir intentando si han ocurrido errores de ingreso*/
-        form.reset();
-    } else {
-        //aviso de error en consola
-        console.log("No has ingresado todos los numeros o NO has dejando espacios en blanco.");
-        /*aviso de error en forma de pista para el usuario: se muestra porque la cadena string
+    form.reset();
+  } else {
+    //aviso de error en consola
+    console.log(
+      "No has ingresado todos los numeros o NO has dejando espacios en blanco."
+    );
+    /*aviso de error en forma de pista para el usuario: se muestra porque la cadena string
         del input no concuerda con el patrón establecido.*/
-        sm_noMatch.classList.remove('hide');
-    }
+    sm_noMatch.classList.remove("hide");
+  }
 }
 
 //Metodo que contiene el algoritmo de Luhn para validación de tarjeta
 /*se recibe el parametro que contiene el string de numeros ya listos
   para comenzar a trabajar con ellos*/
 function resultado(digitList) {
-    /*Obtener el numero de 16 digitos de la tarjeta (19 espacios con el espacio cada 4 digitos)
+  /*Obtener el numero de 16 digitos de la tarjeta (19 espacios con el espacio cada 4 digitos)
     desde el input e ingresarlo a un arreglo.*/
-        
-    //aqui se obtiene la longitud del string que contiene los digitos
-    let strlength = digitList.length;
-        
-    /*Convertimos la cadena string en un arreglo con la propiedad "Array.from()"*/
-    let arreglo = [];
-    arreglo = Array.from(digitList);
-    
-    //se eliminan los espacios vacios del arreglo
-    /*Para eso se divide el arreglo en cuatro grupos sin tomar en cuenta los
+
+  //aqui se obtiene la longitud del string que contiene los digitos
+  let strlength = digitList.length;
+
+  /*Convertimos la cadena string en un arreglo con la propiedad "Array.from()"*/
+  let arreglo = [];
+  arreglo = Array.from(digitList);
+
+  //se eliminan los espacios vacios del arreglo
+  /*Para eso se divide el arreglo en cuatro grupos sin tomar en cuenta los
     espacios para luego unir los cuatro grupos y formar un solo arreglo*/
-    let groupArray1 = arreglo.slice(0, 4);
-    let groupArray2 = arreglo.slice(5, 9);
-    let groupArray3 = arreglo.slice(10, 14);
-    let groupArray4 = arreglo.slice(15, 19);
+  let groupArray1 = arreglo.slice(0, 4);
+  let groupArray2 = arreglo.slice(5, 9);
+  let groupArray3 = arreglo.slice(10, 14);
+  let groupArray4 = arreglo.slice(15, 19);
 
-    /*Union de grupo de arreglos en uno solo */
-    let luhnArray = []
-    //se concatenan los grupos de arreglos en uno solo
-    luhnArray = groupArray1.concat(groupArray2, groupArray3, groupArray4);
-    testArray = groupArray1.concat(groupArray2, groupArray3, groupArray4);
-    //Se invierte el orden del arreglo
-    luhnArray.reverse();
-    
-    //Se evaluan todas las posiciones del arreglo
-    let sumaPares = 0;
-	for (let i = 0; i < luhnArray.length; i++) {
-        let pIndicador = luhnArray[i];
-        
-        //Si la posicion es par, se multiplica el contenido por 2
-		if ((i % 2) != 0) {
-            pIndicador *= 2;
+  /*Union de grupo de arreglos en uno solo */
+  let luhnArray = [];
+  //se concatenan los grupos de arreglos en uno solo
+  luhnArray = groupArray1.concat(groupArray2, groupArray3, groupArray4);
+  testArray = groupArray1.concat(groupArray2, groupArray3, groupArray4);
+  //Se invierte el orden del arreglo
+  console.log(testArray);
+  luhnArray.reverse();
 
-            /*Si el resultado es mayor a 9, se suman ambas cifras
+  //Se evaluan todas las posiciones del arreglo
+  let sumaPares = 0;
+  for (let i = 0; i < luhnArray.length; i++) {
+    let pIndicador = luhnArray[i];
+
+    //Si la posicion es par, se multiplica el contenido por 2
+    if (i % 2 != 0) {
+      pIndicador *= 2;
+
+      /*Si el resultado es mayor a 9, se suman ambas cifras
             de la cantidad obtenida*/
-			if (pIndicador > 9) {
-				pIndicador -= 9;
-			}
-		}
-        sumaPares += pIndicador;
+      if (pIndicador > 9) {
+        pIndicador -= 9;
+      }
     }
+    sumaPares += pIndicador;
+  }
 
-    //return ((sumaPares % 10) == 0);
+  //return ((sumaPares % 10) == 0);
 
-    let totalArray = [];
-    totalArray = Array.from(sumaPares);
+  let totalArray = [];
+  totalArray = Array.from(sumaPares);
 
-    /*el resultado (la suma del los numero del arreglo), se 
+  /*el resultado (la suma del los numero del arreglo), se 
     obtiene mediante una funcion que se manda a llamar.*/
-    let nFinal = sum(totalArray);
+  let nFinal = sum(totalArray);
 
-    //console.log(nFinal);
-    //console.log((nFinal % 10) == 0);
+  //console.log(nFinal);
+  //console.log((nFinal % 10) == 0);
 
-/*Pista visual que indica si el numero es real o no*/
-    
-    /*se verifica si el residuo de la división entre "10"
+  /*Pista visual que indica si el numero es real o no*/
+
+  /*se verifica si el residuo de la división entre "10"
     del numero ubtenido termina en "0"; si es asi, es un numero de tarjeta de credito valido (output:"true"),
     de lo contrario es invalido(output:"false")*/
-    if ((nFinal % 10) == 0) {
-        form.classList.add('hide');
-        titulo.innerText = "La tarjeta que termina en: "+ groupArray4 +" es válida.";
-        titulo.classList.add('input-success');
-        btnReload.classList.toggle('hide');
-    } else {
-        form.classList.add('hide');
-        titulo.innerText = "El numero de tarjeta ingresado "+ testArray +" es inválido, Por favor revise.";
-        titulo.classList.add('input-error');
-        btnReload.classList.toggle('hide');
-    }
-    
+  if (nFinal % 10 == 0) {
+    form.classList.add("hide");
+    titulo.innerText =
+      "La tarjeta que termina en: " + groupArray4 + " es válida.";
+    titulo.classList.add("input-success");
+    btnReload.classList.toggle("hide");
+  } else {
+    form.classList.add("hide");
+    titulo.innerText =
+      "El numero de tarjeta ingresado " +
+      testArray +
+      " es inválido, Por favor revise.";
+    titulo.classList.add("input-error");
+    btnReload.classList.toggle("hide");
+  }
 }
 
 //Funcion que suma todos los elementos de un array numérico
 /*El parametro es el arreglo, ya sea solo su nombre o las posiciones
 especificas del index.*/
-function sum(input){
-             
- if (toString.call(input) !== "[object Array]")
-    return false;
-      
-    let total =  0;
-    for(let i=0;i<input.length;i++)
-        {                  
-        if(isNaN(input[i])){
-            continue;
-        }
-        total += Number(input[i]);
+function sum(input) {
+  if (toString.call(input) !== "[object Array]") return false;
+
+  let total = 0;
+  for (let i = 0; i < input.length; i++) {
+    if (isNaN(input[i])) {
+      continue;
     }
-    return total;
+    total += Number(input[i]);
+  }
+  return total;
 }
--- script.js
+++ script.js
@@ -1,124 +1,102 @@
 //Declaración de variables
-const btnVerificar = document.getElementById('btnVerificar');
-const btnReload = document.getElementById('btnRetry');
-const numero = document.querySelector('#numTarjeta');
-const form = document.getElementById('myForm');
-const titulo = document.getElementById('titulo_informativo')
-const sm_empty = document.getElementById('smE');
-const sm_noMatch = document.getElementById('smN');
+const btnVerificar = document.getElementById("btnVerificar");
+const btnReload = document.getElementById("btnRetry");
+const numero = document.querySelector("#numTarjeta");
+const form = document.getElementById("myForm");
+const titulo = document.getElementById("titulo_informativo");
+const sm_empty = document.getElementById("smE");
+const sm_noMatch = document.getElementById("smN");
 
 btnReload.addEventListener("click", function (e) {
-    location.reload();
+  location.reload();
 });
 
-btnVerificar.addEventListener("click", function (e) { 
-    e.preventDefault();
-    getNumber();
+btnVerificar.addEventListener("click", function (e) {
+  e.preventDefault();
+  //esta funcion va a contener el algoritmo principal
+  checkEmpty();
 });
-
-//esta funcion va a contener el algoritmo principal
-function getNumber() {
-    checkEmpty();
-}
 
 function checkEmpty() {
-    
-    if (numero.value == "") {
-        //error
-        console.log("No has ingresado nada en el campo.");
-        sm_empty.classList.remove('hide');
-    } else {
-        checkPattern();
-        //se oculta la pista visual de error
-        sm_empty.classList.add('hide');
-    }
+  if (numero.value == "") {
+    //error
+    console.log("No has ingresado nada en el campo.");
+    sm_empty.classList.remove("hide");
+  } else {
+    checkPattern();
+    //se oculta la pista visual de error
+    sm_empty.classList.add("hide");
+  }
 }
 
 function checkPattern() {
-    const digitList = numero.value;
-    
-    if (digitList.match(/^([0-9]{4}[\s][0-9]{4}[\s][0-9]{4}[\s][0-9]{3,})$/)) {
-    
-        sm_noMatch.classList.add('hide');
-        resultado(digitList);
+  const digitList = numero.value;
 
-        form.reset();
-    } else {
-        //aviso de error 
-        console.log("No has ingresado todos los numeros o NO has dejando espacios en blanco.");
-        sm_noMatch.classList.remove('hide');
-    }
+  if (digitList.match(/^([0-9]{4}[\s][0-9]{4}[\s][0-9]{4}[\s][0-9]{3,})$/)) {
+    sm_noMatch.classList.add("hide");
+    resultado(digitList);
+    form.reset();
+  } else {
+    //aviso de error
+    sm_noMatch.classList.remove("hide");
+  }
 }
 
 function resultado(digitList) {
-        
-    let arreglo = [];
-    arreglo = Array.from(digitList);
-    
-    //se eliminan los espacios vacios del arreglo
-    let groupArray1 = arreglo.slice(0, 4);
-    let groupArray2 = arreglo.slice(5, 9);
-    let groupArray3 = arreglo.slice(10, 14);
-    let groupArray4 = arreglo.slice(15, 19);
-
-    /*Union de grupo de arreglos en uno solo */
-    let luhnArray = []
-    luhnArray = groupArray1.concat(groupArray2, groupArray3, groupArray4);
-    testArray = groupArray1.concat(groupArray2, groupArray3, groupArray4);
-
-    //Se invierte el orden del arreglo
-    luhnArray.reverse();
-    
-    //Se evaluan todas las posiciones del arreglo
-    let sumaPares = 0;
-	for (let i = 0; i < luhnArray.length; i++) {
-        let pIndicador = luhnArray[i];
-        
-        //Si la posicion es par, se multiplica el contenido por 2
-		if ((i % 2) != 0) {
-            pIndicador *= 2;
-
-            /*Si el resultado es mayor a 9, se suman ambas cifras
+  let arreglo = [];
+  arreglo = Array.from(digitList);
+  let cola = arreglo.slice(15, 19);
+  /*Union de grupo de arreglos en uno solo */
+  let luhnArray = [];
+  luhnArray = arreglo.filter((el) => el != " ");
+  //Se invierte el orden del arreglo
+  luhnArray.reverse();
+  //Se evaluan todas las posiciones del arreglo
+  let sumaPares = [];
+  for (let i = 0; i < luhnArray.length; i++) {
+    let pIndicador = luhnArray[i];
+    //Si la posicion es par, se multiplica el contenido por 2
+    if (i % 2 != 0) {
+      pIndicador *= 2;
+      /*Si el resultado es mayor a 9, se suman ambas cifras
             de la cantidad obtenida*/
-			if (pIndicador > 9) {
-				pIndicador -= 9;
-			}
-		}
-        sumaPares += pIndicador;
+      if (pIndicador > 9) {
+        pIndicador -= 9;
+      }
     }
+    sumaPares += pIndicador;
+  }
 
-    let totalArray = [];
-    totalArray = Array.from(sumaPares);
+  let totalArray = [];
+  totalArray = Array.from(sumaPares);
+  let nFinal = sum(totalArray);
 
-    let nFinal = sum(totalArray);
-
-    if ((nFinal % 10) == 0) {
-        form.classList.add('hide');
-        titulo.innerText = "La tarjeta que termina en: "+ groupArray4 +" es válida.";
-        titulo.classList.add('input-success');
-        btnReload.classList.toggle('hide');
-    } else {
-        form.classList.add('hide');
-        titulo.innerText = "El numero de tarjeta ingresado "+ testArray +" es inválido, Por favor revise.";
-        titulo.classList.add('input-error');
-        btnReload.classList.toggle('hide');
-    }
-    
+  if (nFinal % 10 == 0) {
+    form.classList.add("hide");
+    titulo.innerText = "La tarjeta que termina en: " + cola + " es válida.";
+    titulo.classList.add("input-success");
+    btnReload.classList.toggle("hide");
+  } else {
+    form.classList.add("hide");
+    titulo.innerText =
+      "El numero de tarjeta ingresado " +
+      luhnArray.reverse() +
+      " es inválido, Por favor revise.";
+    titulo.classList.add("input-error");
+    btnReload.classList.toggle("hide");
+  }
 }
 
 //suma de elementos del array
-function sum(input){
-             
- if (toString.call(input) !== "[object Array]")
-    return false;
-      
-    let total =  0;
-    for(let i=0;i<input.length;i++)
-        {                  
-        if(isNaN(input[i])){
-            continue;
-        }
-        total += Number(input[i]);
+function sum(input) {
+  if (toString.call(input) !== "[object Array]") return false;
+
+  let total = 0;
+  for (let i = 0; i < input.length; i++) {
+    if (isNaN(input[i])) {
+      continue;
     }
-    return total;
+    total += Number(input[i]);
+  }
+  return total;
 }
